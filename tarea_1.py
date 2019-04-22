894d63d5bd68663b911bcde734d5725f3148b2b7
carlosrios77 <49490906+carlosrios77@users.noreply.github.com>
2019-04-21 19:37:55 -0600

    Add files via upload

---
+++ tarea_1.py
@@ -0,0 +1,7 @@
+floatValue = 3.1415926
+print("* %.7f" % floatValue)
+print("** %.6f" % floatValue)
+print("*** %.5f" % floatValue)
+print("**** %.4f" % floatValue)
+print("***** %.3f" % floatValue)
+print("****** %.2f" % floatValue)
